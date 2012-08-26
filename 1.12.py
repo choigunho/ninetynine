646b86f2a69d82a347905013ae46d3f8bdb4996f
choi gunho <choiguenho@gmail.com>
2012-08-26 14:35:25 +0900

    .

--- 1.12.py
+++
@@ -1,9 +0,0 @@
-import itertools
-def decode(l):
-	result = []
-
-	for k, g in itertools.groupby(l):
-		result.extend(k[1]*k[0])
-	return result
-
-print decode([[4, 'a'], [1, 'b'], [2, 'c'], [2, 'a'], [1, 'd'], [4, 'e']])
