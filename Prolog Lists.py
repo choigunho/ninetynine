c9dcd56e52f81d36fd5ab772019f853d1d3208d5
choigunho <choiguenho@gmail.com>
2014-02-23 21:23:58 +0900

    1.25

--- Prolog Lists.py
+++ Prolog Lists.py
@@ -184,6 +184,16 @@ class PrologLists(unittest.TestCase):
 
 		print rnd_select2(6, 49)
 
+	'''
+	1.25 (*) Generate a random permutation of the elements of a list.
+		Example:
+		?- rnd_permu([a,b,c,d,e,f],L).
+		L = [b,a,d,c,e,f]
+
+		Hint: Use the solution of problem 1.23.
+	'''
+	def test_1_25(self):
+		print "aaa"
 	
 	
 if __name__ == "__main__":
